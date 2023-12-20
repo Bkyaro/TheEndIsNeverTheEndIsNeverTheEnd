7c736f90d155a83d7e80a7d134bc07e3011be152
Jackal_lzt <bkyarokun@gmail.com>
2023-12-20 16:12:48 +0800

    2-AddTwoNumbers

--- __tests__/1-10/1-10.test.js
+++ __tests__/1-10/1-10.test.js
@@ -1,4 +1,5 @@
 const { twoSum } = require("../../leetcode/1-10/1-TwoSum.js");
+const { addTwoNumbers } = require("../../leetcode/1-10/2-AddTwoNumbers.js");
 describe("1-Two Sums", () => {
   const testy = [
     { nums: [2, 7, 11, 15], target: 9, output: [0, 1] },
@@ -9,6 +10,21 @@ describe("1-Two Sums", () => {
   testy.forEach((test, index) => {
     it(`test${index}`, () => {
       expect(twoSum(test.nums, test.target)).toEqual(test.output);
+    });
+  });
+});
+
+
+describe("2-Add two numbers", () => {
+  const testy = [
+    { l1: [2,4,3], l2: [5,6,4], output: [7,0,8] },
+    { l1: [0], l2: [0], output: [0] },
+    { l1: [9,9,9,9,9,9,9], l2: [9,9,9,9], output: [8,9,9,9,0,0,0,1] }
+  ];
+
+  testy.forEach((test, index) => {
+    it(`test${index}`, () => {
+      expect(addTwoNumbers(test.l1, test.l2)).toEqual(test.output);
     });
   });
 });
--- leetcode/1-10/2-AddTwoNumbers.js
+++ leetcode/1-10/2-AddTwoNumbers.js
@@ -29,4 +29,8 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {};
+var addTwoNumbers = function (l1, l2) {
+  
+};
+
+exports.addTwoNumbers = addTwoNumbers;
