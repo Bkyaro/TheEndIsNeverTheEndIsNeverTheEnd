6141703e08c5c34e0c5ed7c72d70240f09d54648
Jackal_lzt <bkyarokun@gmail.com>
2023-12-25 14:00:11 +0800

    04 add bestiest on leetcode

--- leetcode/1-10/4-MedianOfTwoSortedArrays.js
+++ leetcode/1-10/4-MedianOfTwoSortedArrays.js
@@ -44,25 +44,82 @@ var findMedianSortedArrays = function (nums1, nums2) {
 };
 
 /**
- * method2
+ * Fastest on leetcode
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays2 = function (nums1, nums2) {
-  return !nums1.length
-    ? findMediant(nums2)
-    : !nums2.length
-    ? findMediant(nums1)
-    : findMediant([...nums1, ...nums2].sort((a, b) => a - b));
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  let arr = nums1.concat(nums2);
+  arr = arr.sort((a, b) => a - b);
+  let l = arr.length;
 
-  function findMediant(arr) {
-    if (arr.length % 2 == 1) {
-      return arr[Math.floor(arr.length / 2)];
+  let median = 0;
+  if (l % 2 !== 0) {
+    median = arr[(l - 1) / 2];
+  } else {
+    let medInd = arr[(l - 2) / 2] + arr[l / 2];
+    median = medInd / 2;
+  }
+  return median;
+};
+/**
+ * Smallest on leetcode
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  // Ensure nums1 is the smaller array
+  if (nums1.length > nums2.length) {
+    [nums1, nums2] = [nums2, nums1];
+  }
+
+  const m = nums1.length;
+  const n = nums2.length;
+
+  let low = 0;
+  let high = m;
+
+  while (low <= high) {
+    const partitionX = Math.floor((low + high) / 2);
+    const partitionY = Math.floor((m + n + 1) / 2) - partitionX;
+
+    const maxX =
+      partitionX === 0 ? Number.NEGATIVE_INFINITY : nums1[partitionX - 1];
+    const minX =
+      partitionX === m ? Number.POSITIVE_INFINITY : nums1[partitionX];
+
+    const maxY =
+      partitionY === 0 ? Number.NEGATIVE_INFINITY : nums2[partitionY - 1];
+    const minY =
+      partitionY === n ? Number.POSITIVE_INFINITY : nums2[partitionY];
+
+    if (maxX <= minY && maxY <= minX) {
+      // Found the correct partition
+      if ((m + n) % 2 === 0) {
+        // Even total length
+        return (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2;
+      } else {
+        // Odd total length
+        return Math.max(maxX, maxY);
+      }
+    } else if (maxX > minY) {
+      // Move to the left in nums1
+      high = partitionX - 1;
     } else {
-      return (arr[arr.length / 2] + arr[arr.length / 2 - 1]) / 2;
+      // Move to the right in nums1
+      low = partitionX + 1;
     }
   }
+
+  // This line is added to handle the case where the arrays are not sorted
+  throw new Error("Input arrays are not sorted!");
 };
 
 module.exports = findMedianSortedArrays;
