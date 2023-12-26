4ce689de385140912e2cbb05985eb82b1228dc1a
Jackal_lzt <bkyarokun@gmail.com>
2023-12-26 14:44:46 +0800

    5 Longest palindromic substring top answer

--- leetcode/1-10/5-LongestPalindromicSubstring.js
+++ leetcode/1-10/5-LongestPalindromicSubstring.js
@@ -49,4 +49,81 @@ var longestPalindrome = function (s) {
   return result;
 };
 
+/**
+ * fastest on leetcode
+ * @param {string} s
+ * @return {string}
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (s === [...s].reverse().join("")) {
+    return s;
+  }
+
+  let result = s[0];
+
+  for (let i = 0; i < s.length; i += 1) {
+    let l1 = i;
+    let r1 = i;
+    let oddStr = "";
+
+    let l2 = i;
+    let r2 = i + 1;
+    let evenStr = "";
+
+    while (l1 >= 0 && r1 < s.length && s[l1] === s[r1]) {
+      l1--;
+      r1++;
+    }
+
+    while (l2 >= 0 && r2 < s.length && s[l2] === s[r2]) {
+      l2--;
+      r2++;
+    }
+
+    oddStr = s.slice(l1 + 1, r1);
+    evenStr = s.slice(l2 + 1, r2);
+
+    if (oddStr.length > result.length) {
+      result = oddStr;
+    }
+
+    if (evenStr.length > result.length) {
+      result = evenStr;
+    }
+  }
+
+  return result;
+};
+
+/**
+ * lightest on leetcode
+ * @param {string} s
+ * @return {string}
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  let str = "";
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < 2; j++) {
+      let start = i;
+      let end = i + j;
+      while (s[start] && s[start] === s[end]) {
+        start -= 1;
+        end += 1;
+      }
+      if (end - start - 1 > str.length) {
+        str = s.slice(start + 1, end);
+      }
+    }
+  }
+  return str;
+};
+
 module.exports = longestPalindrome;
