7388e11b89a27beac4e358cd478e61896fa51dbf
Bkyaro <30712768+Bkyaro@users.noreply.github.com>
2023-12-28 19:41:45 +0800

    06 zigzag convertion random solution on gh

--- leetcode/1-10/6-ZigzagConvertion.js
+++ leetcode/1-10/6-ZigzagConvertion.js
@@ -79,4 +79,23 @@ var convert = function (s, numRows) {
   return store.flat().join("");
 };
 
+/**
+ * random solution on github
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  if(numRows === 1 || s.length <= numRows) return s;
+  const grid = Array(numRows).fill('');
+  let i = 0, down = true;
+  
+  for(let c of s) {
+      grid[i] += c;
+      down ? i++ : i--;
+      if(i === numRows-1 || !i) down = !down;
+  }
+  return grid.join('')
+};
+
 module.exports = convert;
