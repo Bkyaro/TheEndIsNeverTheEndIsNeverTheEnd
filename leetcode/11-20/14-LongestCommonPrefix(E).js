d12ac471a7dfb2871bcbc4eebb6fd05f8b110b09
Jackal_lzt <bkyarokun@gmail.com>
2024-01-10 14:25:45 +0800

    14 Longest common prefix

--- leetcode/11-20/14-LongestCommonPrefix(E).js
+++ leetcode/11-20/14-LongestCommonPrefix(E).js
@@ -30,4 +30,28 @@ var longestCommonPrefix = function (strs) {
   }
   return ans;
 };
+
+/**
+ * fastest on gh
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  let prefix = "";
+  const firstStr = strs[0];
+
+  for (let i = 0; i < firstStr.length; i++) {
+    const currChar = firstStr[i];
+
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j][i] !== currChar) {
+        return prefix;
+      }
+    }
+
+    prefix += currChar;
+  }
+
+  return prefix;
+};
 module.exports = longestCommonPrefix;
